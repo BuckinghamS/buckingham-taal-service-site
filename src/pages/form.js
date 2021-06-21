9ba762572c3773a3f47abbfd49dd8d39cdc13f00
Robbert Tuerlings <robberttg@protonmail.com>
2021-06-21 21:11:46 +0200

    Update form for mobile

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { ButtonContainer, Form, Hero, Wrapper } from '../styles/contactStyles'
+import { ButtonContainer, Form, Hero, SuccessContainer, Wrapper } from '../styles/formStyles'
 import { Button } from '../components/Elements'
 import { useForm, ValidationError } from '@formspree/react'
 import { AiOutlineCheckCircle } from 'react-icons/ai'
 
+const FORM_ID = process.env.GATSBY_STATIC_FORM_ACCESS_KEY
 const FORM_HEADER = 'Offerteformulier'
 const FORM_SUB_HEADER = 'Vraag hier uw offerte aan!'
 const NAME = 'Naam'
@@ -27,29 +28,31 @@ const THANK_YOU =
   'Het formulier is succesvol verstuurd. Er wordt zo spoedig mogelijk contact met u opgenomen.'
 
 function ContactForm() {
-  const [state, handleSubmit] = useForm('xayajlnn')
+  // const [state, handleSubmit] = useForm('xayajlnn')
+    const [state, handleSubmit] = useForm(FORM_ID)
   if (state.succeeded) {
     return (
       <Form>
-        <div>
+        <SuccessContainer>
           <AiOutlineCheckCircle size={48} />
-          <p className="landingpage-p">{THANK_YOU}</p>
-        </div>
+          <p className="landingpage-p" style={{marginTop: '0.5rem', textAlign: 'center'}}>{THANK_YOU}</p>
+        </SuccessContainer>
       </Form>
     )
   }
+
   return (
     <Form onSubmit={handleSubmit}>
-      <label for="full-name">{NAME}</label>
+      <label htmlFor="full-name">{NAME}</label>
       <input type="text" name="naam" id="full-name" placeholder={PH_NAME} required="yes"></input>
       <label htmlFor="email">{EMAIL}</label>
       <input id="email" type="email" name="email" placeholder={PH_EMAIL} required="yes" />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
       <input type="text" name="_gotcha" style={{ display: 'none' }} />
-      <label for="telephone">{PHONE}</label>
+      <label htmlFor="telephone">{PHONE}</label>
       <input type="telephone" name="telefoon" id="telephone" placeholder={PH_PHONE}></input>
       {/* <fieldset id="fs-frm-selects"> */}
-      <label for="project_type">{PROJECT_TYPE}</label>
+      <label htmlFor="project_type">{PROJECT_TYPE}</label>
       <select name="project" id="project_type" required="yes">
         <option value="Choose" selected="" disabled="">
           {OPTION_HEADER}
@@ -60,12 +63,12 @@ function ContactForm() {
         <option value="7">{REVISION}</option>
       </select>
       {/* </fieldset> */}
-      <label for="message">{DESCRIPTION}</label>
+      <label htmlFor="message">{DESCRIPTION}</label>
       <textarea rows="3" id="message" name="omschrijving" required="" />
       <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <label for="word-count">{WORD_COUNT}</label>
+      <label htmlFor="word-count">{WORD_COUNT}</label>
       <input type="text" name="woorden" id="word-count" required=""></input>
-      <label for="deadline">{DEADLINE}</label>
+      <label htmlFor="deadline">{DEADLINE}</label>
       <input type="text" name="deadline" id="deadline" required="yes"></input>
       <ButtonContainer>
         <Button type="submit" disabled={state.submitting}>
